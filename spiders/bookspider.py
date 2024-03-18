253e3a29672648d01e09c039ce95cfbc258f4517
Louis Moises <louis@moises.fr>
2024-03-18 12:23:21 +0100

    demarrage

---
+++ spiders/bookspider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class BookSpider(scrapy.spider):
+    titre = ''
+    titre_original =
+    score =
+    genre =
+    année =
+    durée =
+    descriptions =
+    acteurs =
+    public =
+    pays =
+    langue_origine = 
+
+def start_requests(self):
+    url = 
+    
